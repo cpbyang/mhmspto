a33d021af3e1d10d89c7011d80200edac86f98fd
cpbyang <byang@cafepress.com>
2015-01-29 11:03:18 -0800

    Initial deployment of MHMS PTO website into GIT

---
+++ BootstrapMVC/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Board Members";
+
+            return View();
+        }
+
+        public ActionResult Events()
+        {
+            ViewBag.Message = "Upcoming Events";
+
+            return View();
+        }
+
+        public ActionResult Calendar()
+        {
+            ViewBag.Message = "Calendar";
+
+            return View();
+        }
+
+        public ActionResult Documents()
+        {
+            ViewBag.Message = "Documents";
+
+            return View();
+        }
+
+        public ActionResult Committees()
+        {
+            ViewBag.Message = "Committees";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
